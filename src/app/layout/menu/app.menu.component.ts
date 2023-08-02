9efb2c1eb2a7f10d07170a6d48e4cdc66fde4392
RicardoQuishpe <ftamay69@gmail.com>
2023-08-02 11:19:25 -0500

    rutas

--- src/app/layout/menu/app.menu.component.ts
+++ src/app/layout/menu/app.menu.component.ts
@@ -108,6 +108,11 @@ export class AppMenuComponent implements OnInit {
                     icon: 'pi pi-check',
                     routerLink: ['/nota-credito'],
                 },
+                {
+                    label: 'Factura Otros Conceptos',
+                    icon: 'pi pi-check',
+                    routerLink: ['/otros_conceptos'],
+                },
             ],
         },
     ];
