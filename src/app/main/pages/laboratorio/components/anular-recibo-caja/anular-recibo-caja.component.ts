cf76cff2653ff446919d31f0251a43ccea7f0a6e
Kevin R <shimpo11.kr@gmail.com>
2023-08-02 16:03:28 -0500

    -> Estructura del componente formulario y componernte tabla de anular recibos

--- src/app/main/pages/laboratorio/components/anular-recibo-caja/anular-recibo-caja.component.ts
+++ src/app/main/pages/laboratorio/components/anular-recibo-caja/anular-recibo-caja.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
+import { ResponseGenerico } from 'src/app/_dto/response-generico';
+import { TokenDto } from 'src/app/_dto/token-dto';
+import { TokenService } from 'src/app/_service/token.service';
+import { BreadcrumbService } from 'src/app/_service/utils/app.breadcrumb.service';
+import { ReciboCaja } from '../../model/reciboCaja';
 
 @Component({
     selector: 'app-anular-recibo-caja',
@@ -6,13 +17,58 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./anular-recibo-caja.component.scss'],
 })
 export class AnularReciboCajaComponent implements OnInit {
+    @Input() reciboCaja: ReciboCaja; //Va reciboDTo
+    reciboCajaFiltrados: ReciboCaja; //va ReciboDto
 
-    fechaDesde: Date;
-    fechaHasta:Date;
+    proceso: string = 'anular recibos caja';
+    response: ResponseGenerico;
+    token: TokenDto;
+    anularRecibCajaForm: FormGroup;
 
-    constructor() {}
+    constructor(
+        private breadcrumbService: BreadcrumbService,
+        private formBuilder: FormBuilder,
+        private tokenService: TokenService
+    ) {
+        {
+            this.breadcrumbService.setItems([{ label: 'Anular Recibo Caja' }]);
+        }
+    }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.iniciarForms();
+    }
 
-    Buscar() {}
+    get f() {
+        return this.anularRecibCajaForm.controls;
+    }
+
+    iniciarForms() {
+        this.anularRecibCajaForm = this.formBuilder.group({
+            //idEstadoComprobante: [null],
+            NroReciboCaja: ['', Validators.required],
+            NombreCliente: ['', Validators.required],
+            Ruc: ['', Validators.required],
+            Cedula: ['', Validators.required],
+            fechaDesde: ['', Validators.required],
+            fechaHasta: ['', Validators.required],
+            //estadoCompr: [true, Validators.requiredTrue],
+        });
+        this.token = JSON.parse(this.tokenService.getResponseAuth());
+        //this.f.idUsuarioEstComprob.setValue(this.token.id)
+    }
+
+    obtenerdaData() {}
+
+    Buscar() {
+        const fechaDesde = this.anularRecibCajaForm.value.fechaDesde;
+        const fechaHasta = this.anularRecibCajaForm.value.fechaDesde;
+        console.log('filtrando info: ' + fechaDesde);
+        console.log('filtrando info  hasta: ' + fechaHasta);
+
+        /*  this.reciboCaja = this.reciboCaja.filter((recibo) => {
+            const fechaRecibo = new Date(recibo.fecha);
+            return fechaRecibo >= fechaDesde && fechaRecibo <= fechaHasta;
+        }); */
+    }
 }
--- src/app/main/pages/laboratorio/module/entidad.module.ts
+++ src/app/main/pages/laboratorio/module/entidad.module.ts
@@ -1,42 +1,41 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from "@angular/router";
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from "primeng/button";
-import {CalendarModule} from "primeng/calendar";
-import {TooltipModule} from "primeng/tooltip";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { TooltipModule } from 'primeng/tooltip';
 
-
-import {RUTA_ENTIDAD} from "../routes/entidad.routing";
-import {DataViewModule} from "primeng/dataview";
-import {DropdownModule} from "primeng/dropdown";
+import { RUTA_ENTIDAD } from '../routes/entidad.routing';
+import { DataViewModule } from 'primeng/dataview';
+import { DropdownModule } from 'primeng/dropdown';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PanelModule } from 'primeng/panel';
-import {StyleClassModule} from "primeng/styleclass";
-import {RippleModule} from "primeng/ripple";
+import { StyleClassModule } from 'primeng/styleclass';
+import { RippleModule } from 'primeng/ripple';
 
-import {ToolbarModule} from "primeng/toolbar";
-import {TableModule} from "primeng/table";
-import {DividerModule} from "primeng/divider";
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
+import { DividerModule } from 'primeng/divider';
 
-import {InputTextareaModule} from "primeng/inputtextarea";
-import {FileUploadModule} from "primeng/fileupload";
-import {InputNumberModule} from "primeng/inputnumber";
-import {DialogModule} from "primeng/dialog";
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogModule } from 'primeng/dialog';
 
-import {SpeedDialModule} from "primeng/speeddial";
-import {AutoCompleteModule} from "primeng/autocomplete";
-import {PrimengModule} from "src/app/primeng/primeng.module";
+import { SpeedDialModule } from 'primeng/speeddial';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { PrimengModule } from 'src/app/primeng/primeng.module';
 
-import {ScrollPanelModule} from 'primeng/scrollpanel';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
-import {EntidadComponent} from "../components/entidad/entidad.component";
-import {EntidadTableComponent} from "../components/entidad-table/entidad-table.component";
+import { EntidadComponent } from '../components/entidad/entidad.component';
+import { EntidadTableComponent } from '../components/entidad-table/entidad-table.component';
 
-import {CdkTableModule} from "@angular/cdk/table";
-import {BancosComponent} from "../components/bancos/bancos.component";
-import {BancosTableComponent } from "../components/bancos-table/bancos-table.component";
+import { CdkTableModule } from '@angular/cdk/table';
+import { BancosComponent } from '../components/bancos/bancos.component';
+import { BancosTableComponent } from '../components/bancos-table/bancos-table.component';
 import { ConceptoComponent } from '../components/concepto/concepto.component';
 import { FormaPagoComponent } from '../components/forma-pago/forma-pago.component';
 import { ConceptosTableComponent } from '../components/concepto-table/concepto-table.component';
@@ -61,9 +60,6 @@ import { FacturaMatriculaTableComponent } from '../components/factura-matricula-
 import { NotaCreditoComponent } from '../components/nota-credito/nota-credito.component';
 import { NotaCreditoTableComponent } from '../components/nota-credito-table/nota-credito-table.component';
 
-
-
-
 @NgModule({
     declarations: [
         EntidadComponent,
@@ -86,9 +82,9 @@ import { NotaCreditoTableComponent } from '../components/nota-credito-table/nota
         FacturaLaboratorioComponent,
         FacturaLaboratorioTableComponent,
         FacturaMatriculaComponent,
-       FacturaMatriculaTableComponent,
-       NotaCreditoComponent,
-       NotaCreditoTableComponent
+        FacturaMatriculaTableComponent,
+        NotaCreditoComponent,
+        NotaCreditoTableComponent,
     ],
 
     imports: [
@@ -119,8 +115,7 @@ import { NotaCreditoTableComponent } from '../components/nota-credito-table/nota
         SpeedDialModule,
         ScrollPanelModule,
         PrimengModule,
-        CdkTableModule
-    ]
+        CdkTableModule,
+    ],
 })
-export class EntidadModule {
-}
+export class EntidadModule {}
