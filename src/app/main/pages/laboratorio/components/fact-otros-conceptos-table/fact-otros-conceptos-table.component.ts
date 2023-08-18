3bac7e3c91f964e614f6798b12a5da03d522e8ff
michael7543 <mdr.moran@yavirac.edu.ec>
2023-08-18 10:57:21 -0500

    cambios factura otros conceptos

--- src/app/main/pages/laboratorio/components/fact-otros-conceptos-table/fact-otros-conceptos-table.component.ts
+++ src/app/main/pages/laboratorio/components/fact-otros-conceptos-table/fact-otros-conceptos-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FacturaService } from '../../services/factura.service';
+import { AppService } from 'src/app/_service/app.service';
+import { severities } from 'src/app/_enums/constDomain';
 
 @Component({
   selector: 'app-fact-otros-conceptos-table',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fact-otros-conceptos-table.component.css']
 })
 export class FactOtrosConceptosTableComponent implements OnInit {
+  @Input() listfacturaotrosconceptos:  any [] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private facturaService: FacturaService,
+    public appService: AppService,
+  ) { 
+    
   }
 
+  ngOnInit() {
+    //this.llenarFacturalaboratorio();
+  }
+
+  filtrarFacturas(listfacturaotrosconceptos:any[]){
+    this.listfacturaotrosconceptos = listfacturaotrosconceptos
+  }
+
+  
+  async llenarFacturalaboratorio() {
+    await this.facturaService.getAll().subscribe({
+        next: (data) => {
+            this.listfacturaotrosconceptos = data.listado;
+            console.log('CORRECTO');
+            console.log(this.listfacturaotrosconceptos);
+        },
+        complete: () => {
+            this.appService.msgInfoDetail(
+                severities.INFO,
+                'INFO',
+                'Datos Cargados exitosamente' ,
+                500
+            );
+        },
+        error: (error) => {
+            this.appService.msgInfoDetail(
+                severities.ERROR,
+                'ERROR',
+                error.error
+            );
+        },
+    });
+  }
 }
