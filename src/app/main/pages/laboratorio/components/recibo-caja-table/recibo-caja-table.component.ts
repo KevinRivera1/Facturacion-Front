8b093aeb7d0201d7c6b93fc32507f03f424174ff
M0nt0ya <montoya0andres@gmail.com>
2023-08-22 11:47:28 -0500

    Andres

--- src/app/main/pages/laboratorio/components/recibo-caja-table/recibo-caja-table.component.ts
+++ src/app/main/pages/laboratorio/components/recibo-caja-table/recibo-caja-table.component.ts
@@ -9,6 +9,8 @@ import { severities } from 'src/app/_enums/constDomain';
 import { AppService } from 'src/app/_service/app.service';
 import { FileService } from 'src/app/_service/utils/file.service';
 import { ReciboCajaService } from '../../services/reciboCaja.service';
+import { ReciboCajaDto } from '../../model/reciboCajaDto';
+import jsPDF from 'jspdf';
 
 @Component({
     selector: 'app-recibo-caja-table',
@@ -19,8 +21,9 @@ export class ReciboCajaTableComponent implements OnInit {
     proceso: string = 'anular Recibo caja';
     displayModal: boolean = false;
 
-    @Input() recibos: any[] = [];
+    @Input() recibos : ReciboCajaDto [] =[];;
 
+    
     submitted: boolean;
     loading: boolean;
     exportColumns: any[];
@@ -74,6 +77,45 @@ export class ReciboCajaTableComponent implements OnInit {
     closeModal() {
         this.displayModal = false;
         console.log('cerrando modal');
+    }
+
+
+    exportPdf() {
+        let indexLista: number = 0;
+        this.recibos.forEach((element) => {
+            indexLista++;
+            element.idReciboCaja = indexLista;
+        });
+        this.appService.exportPdf(
+            this.exportColumns,
+            this.recibos,
+            'Centro de Costos',
+            'p'
+        );
+    }
+
+    exportPdfById(idReciboCaja: number) {
+        // Buscar el recibo con el idReciboCaja proporcionado
+        const recibo = this.recibos.find((element) => element.idReciboCaja === idReciboCaja);
+    
+        if (!recibo) {
+            console.log('No se encontró un recibo con el idReciboCaja proporcionado.');
+            return;
+        }
+    
+        // Crear un nuevo documento PDF
+        const doc = new jsPDF();
+    
+        // Agregar contenido al PDF
+        doc.setFontSize(12);
+        doc.text(`Recibo de Caja Nº ${recibo.idReciboCaja}`, 10, 10);
+        doc.text(`Recibo de Caja Nº ${recibo.codRcaja}`, 10, 10);
+        doc.text(`Recibo de Caja Nº ${recibo.nombreConsumidorRc}`, 10, 10);
+
+        // Agregar más contenido aquí
+    
+        // Guardar el PDF
+        doc.save(`recibo_caja_${recibo.codRcaja}.pdf`);
     }
 }
 
