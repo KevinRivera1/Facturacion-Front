31b6d0b7e4be7c7027f6ad76f227a5bff93e171a
michael7543 <mdr.moran@yavirac.edu.ec>
2023-08-22 11:46:15 -0500

    cambios laboratorio

--- src/app/main/pages/laboratorio/components/factura-laboratorio-table/factura-laboratorio-table.component.ts
+++ src/app/main/pages/laboratorio/components/factura-laboratorio-table/factura-laboratorio-table.component.ts
@@ -39,7 +39,7 @@ export class FacturaLaboratorioTableComponent implements OnInit {
 
   ngOnInit() {
     this.clienteSelect= new DetalleFacturaDto();
-    this.llenardetalleFacturalaboratorio();
+   // this.llenardetalleFacturalaboratorio();
   }
 
 
@@ -73,7 +73,6 @@ export class FacturaLaboratorioTableComponent implements OnInit {
     this.clienteSelect= clienteSelectDto;
     this.abrirmodal();
     
-
   }
 
 
@@ -86,15 +85,6 @@ cerrar() {
   
 
 }
-
-/* formatearFecha(fecha: number): string {
-  const date = new Date(fecha);
-  const anio = date.getFullYear();
-  const mes = ('0' + (date.getMonth() + 1)).slice(-2);
-  const dia = ('0' + date.getDate()).slice(-2);
-
-  return `${dia}/${mes}/${anio}`;
-} */
 
 filtrarFacturas(listfacturalaboratorio:any[]){
   this.listfacturalaboratorio = listfacturalaboratorio
