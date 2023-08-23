4ff2f6c794888cc2167babfa516d6613eefb82a3
Francisco Xavier <xavier.yambay@epn.edu.ec>
2023-08-23 12:43:11 -0500

    cambio

--- src/app/main/pages/laboratorio/components/factura-matricula-table/factura-matricula-table.component.ts
+++ src/app/main/pages/laboratorio/components/factura-matricula-table/factura-matricula-table.component.ts
@@ -5,6 +5,7 @@ import { severities } from 'src/app/_enums/constDomain';
 import { AppService } from 'src/app/_service/app.service';
 import { DetalleFacturaDto } from '../../model/DetalleFactura.dto';
 import { DetalleFacturaService } from '../../services/detalleFactura.service';
+import { ReciboCajaDto } from '../../model/reciboCajaDto';
 
 @Component({
   selector: 'app-factura-matricula-table',
@@ -18,6 +19,8 @@ export class FacturaMatriculaTableComponent implements OnInit {
 
   modal: boolean;
   clienteSelect: DetalleFacturaDto;
+  cols: { field: string; header: string; }[];
+  exportColumns: { title: string; dataKey: string; }[];
 
   constructor(
     private detalleFacturaService: DetalleFacturaService,
@@ -61,6 +64,7 @@ export class FacturaMatriculaTableComponent implements OnInit {
         this.loading = false;
       });
     }, 1000);
+    
   }
 
   cargarfactura(clienteSelectDto: DetalleFacturaDto) {
@@ -82,11 +86,49 @@ export class FacturaMatriculaTableComponent implements OnInit {
   ngOnInit() {
     // this.llenarFacturaMatricula();
     this.clienteSelect = new DetalleFacturaDto();
+    this.construirTabla();
 
   }
 
   filtrarFactura(listFactura: any[]) {
     this.listFactura = listFactura
   }
+
+  construirTabla() {
+    this.cols = [
+        //{ field: 'idReciboCaja', header: 'Nro.RECIBO' },
+        { field: 'codRcaja', header: 'Nro.RECIBO' },
+        { field: 'nombreConsumidorRc', header: 'NOMBRE.' },
+        { field: 'rucConsumidorRc', header: 'Ruc.' },
+        { field: 'fechaRcaja', header: 'FECHA.' },
+        { field: 'totalRc', header: 'TOTAL' },
+        { field: 'idEstadoRc', header: 'ESTADO' },
+        { field: 'observacionRc', header: 'MOTIVO' },
+    ];
+    this.exportColumns = this.cols.map((col) => ({
+        title: col.header,
+        dataKey: col.field,
+    }));
+    this.loading = false;
+}
+
+  exportPdf() {
+    if (this.listFactura && this.listFactura.length > 0) {
+        this.listFactura = this.listFactura.map((element, index) => {
+            element.idCajaRc = index + 1;
+            return element;
+        });
+        this.appService.exportPdf(this.exportColumns, this.listFactura, 'Anular Recibo Caja', 'p'
+        );
+    } else {
+        console.log('No hay datos para exportar a PDF.');
+        this.appService.msgInfoDetail(
+            severities.ERROR,
+            'ERROR',
+            'No se encontraron registros para generar el PDF',
+            700
+        );
+    }
+}
 
 }
